c2c71887867cc15ca3c77f5d6527002cb91ac2b4
Jason Louv <jason@ultraculture.org>
2022-05-25 22:13:02 -0500

    Finished popup tutorial

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import {isOverlayOpen} from "../stores/OverlayStore.js";
+	import Overlay from "../components/Overlay.svelte";
+	import '../app.css';
+</script>
+
+<Header />
+
+{#if $isOverlayOpen}
+	<Overlay>
+		<p class="text-white text-4xl">They played us like a <span class="text-red">damn fiddle</span>!</p>
+	</Overlay>
+{/if}
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,62 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import Counter from '$lib/Counter.svelte';
+	import {isOverlayOpen} from '../stores/OverlayStore.js';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+	{$isOverlayOpen}
+	<button class="bg-purple-700 text-purple-100 p-4 rounded-md my-4" on:click={() => {isOverlayOpen.set(true)}}>Open Overlay</button>
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
